0d782cd21252b82d2ed6a0962eefd0eb21441276
joeldenning <joeldenning@gmail.com>
2013-10-07 22:24:52 -0600

    PDController and TankVector fixes. We need to talk about whether we are storing velocities or positions

--- src/agent/pdcontroller/PDController.java
+++ src/agent/pdcontroller/PDController.java
@@ -1,9 +1,11 @@
 package agent.pdcontroller;
 
+import agent.AbstractAgent;
 import agent.potentialfields.TankVector;
 import environment.Action;
 import environment.Environment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,10 +17,38 @@ import java.util.List;
  */
 public class PDController {
 
-    private double lastError;
+	private static final double
+			SPEED_PROPORTIONAL_CONSTANT = 1, SPEED_DERIVATIVE_CONSTANT = 1,
+			ANGULAR_VELOCITY_PROPORTIONAL_CONSTANT = 1, ANGULAR_VELOCITY_DERIVATIVE_CONSTANT = 1;
+
+	private final AbstractAgent agent;
+
+	public PDController(AbstractAgent agent) {
+		this.agent = agent;
+	}
 
     public List<Action> getActions(TankVector desiredVector, Environment environment, long timeElapsed) {
-        //TODO Joel
-        return null;  //To change body of created methods use File | Settings | File Templates.
+        List<Action> actions = new ArrayList<Action>();
+	    actions.add(getSpeedAction(desiredVector, environment, timeElapsed));
+	    actions.add(getAngleAction(desiredVector, environment, timeElapsed));
+	    return actions;
     }
+
+	private Action getAngleAction(TankVector desiredVector, Environment environment, long timeElapsed) {
+		double curAngVel = environment.getMyState().getAngularVelocity();
+		double currentError = desiredVector.getDesiredAngularVelocity() - curAngVel;
+		double actualAngVel =
+				ANGULAR_VELOCITY_PROPORTIONAL_CONSTANT * (desiredVector.getDesiredAngularVelocity()-curAngVel) +
+				ANGULAR_VELOCITY_DERIVATIVE_CONSTANT * (currentError / timeElapsed);
+		return new Action(agent, Action.Type.ANGVEL, Double.toString(actualAngVel));
+	}
+
+	private Action getSpeedAction(TankVector desiredVector, Environment environment, long timeElapsed) {
+		double curSpeed = environment.getMyState().getVx() + environment.getMyState().getVy();
+		double currentError = desiredVector.getDesiredSpeed() - curSpeed;
+		double actualSpeed =
+				SPEED_PROPORTIONAL_CONSTANT * (desiredVector.getDesiredSpeed()-curSpeed) +
+				SPEED_DERIVATIVE_CONSTANT * (currentError / timeElapsed);
+		return new Action(agent, Action.Type.SPEED, Double.toString(actualSpeed));
+	}
 }
