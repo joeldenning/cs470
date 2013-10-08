c5871a22d5cc1404a0ae92b3381d26efb8b0db10
brian <herobd@gmail.com>
2013-10-08 14:46:22 -0600

    Work on field

--- src/agent/potentialfields/AttractiveField.java
+++ src/agent/potentialfields/AttractiveField.java
@@ -13,15 +13,19 @@ import environment.Tank;
  */
 public class AttractiveField extends PotentialField {
 
+	private static final double FLAG_CONST = 1;
+	private static final double BASE_CONST = 1;
 	private double radius;
 	private double goalX;
 	private double goalY;
+	private double alpha;
 	
     public AttractiveField(Flag flag, Tank myself) {
         super(myself);
-        radius = 1;
+        radius = 1;//TODO What should this be?
         goalX = flag.getX();
         goalY = flag.getY();
+        alpha = FLAG_CONST;
     }
 
     public AttractiveField(Base base, Tank myself) {
@@ -39,12 +43,23 @@ public class AttractiveField extends PotentialField {
         goalY = goalY/numOfCorners;
         
         //Rough estimate on radius
+        radius = Math.abs(base.getCorner(0).x - base.getCorner(0).y);
         
+        alpha = BASE_CONST;
     }
 
     @Override
     public TankVector toTankVector() {
         //TODO Brian
+    	
+    	double distance = Math.sqrt(Math.pow(goalX-myself.getX(), 2) + Math.pow(goalY-myself.getY(), 2));
+    	if (distance < radius) {
+    		return new TankVector(0,0);
+    	}
+    	double angle = Math.atan2(goalY-myself.getY(), goalX-myself.getX());
+    	double magnitude = alpha*(distance-radius);
+    	
+    	
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
